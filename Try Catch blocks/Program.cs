3762f1611a35cb301c762ba32e312e35de77c63d
alijo <alijo@ALIJONOV>
2023-12-02 10:13:55 +0500

    Add project files.

---
+++ Try Catch blocks/Program.cs
@@ -0,0 +1,23 @@
+﻿public class Program
+{
+    private static void Main(string[] args)
+    {
+        int n;
+        try
+        {
+            n = int.Parse(Console.ReadLine());
+        }
+        catch(DivideByZeroException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        //catch(DivideByZeroException ex)
+        //{
+        // Bitta Exception'dan qayta foydalanib bo'lmaydi.
+        //}
+        finally
+        {
+            Console.WriteLine("Muammoli qism muvaffaqiyatli tugatildi!");
+        }
+    }
+}
